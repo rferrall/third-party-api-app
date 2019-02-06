850238cf3254e3b3cfe8bd98b9b71daaf2a910ab
rachel <ferrallr@yahoo.com>
2019-02-05 18:18:27 -0600

    hides api key and updates index

--- app/controllers/api/colors_controller.rb
+++ app/controllers/api/colors_controller.rb
@@ -1,7 +1,7 @@
 class Api::ColorsController < ApplicationController
 
   def index
-    @colors = HTTP.get("https://api.harvardartmuseums.org/color?apikey=82682f90-2996-11e9-b19a-9b286143c818").parse["records"]
+    @colors = HTTP.get("https://api.harvardartmuseums.org/color?apikey=#{ENV["API_KEY"]}").parse["records"]
 
     render 'index.json.jbuilder'
     
